ae83a2248ffe0f3934edcddc56bd923efb083c77
javiercarpio57 <javierrr.carpio57@gmail.com>
2020-11-14 17:47:18 -0600

    Movimiento aleatorio de personas

---
+++ Contagios/Assets/Scripts/BrownianMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrownianMovement : MonoBehaviour
+{
+    public Vector3 moveDirection;
+    public float speed = 0.35f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveDirection = new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10));
+    }
+
+    private void Update()
+    {
+        transform.Translate(moveDirection * Time.deltaTime * speed);
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        var wall = collision.gameObject.GetComponent<Wall>();
+
+        if (wall != null)
+        {
+            if ((wall.side == WallSide.LEFT) || (wall.side == WallSide.RIGHT))
+            {
+                moveDirection.x = moveDirection.x * -1;
+            }
+            else if ((wall.side == WallSide.DOWN) || (wall.side == WallSide.UP))
+            {
+                moveDirection.z = moveDirection.z * -1;
+            }
+        }
+
+        var person = collision.gameObject.GetComponent<Person>();
+        if (person != null)
+        {
+            moveDirection = new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10));
+        }
+    }
+}
---
+++ Contagios/Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int CantidadPersonas = 100;
+    public Person person;
+    public int LeftWall;
+    public int RightWall;
+    public int DownWall;
+    public int UpWall;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (var i = 0; i <= CantidadPersonas; i++)
+        {
+            GameObject newPerson = Instantiate(person, null).gameObject;
+            float x = Random.Range(LeftWall, RightWall);
+            float z = Random.Range(DownWall, UpWall);
+            newPerson.transform.position = new Vector3(x, 0.4f, z);
+        }
+    }
+}
